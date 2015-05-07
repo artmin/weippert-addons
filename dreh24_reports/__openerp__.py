74847614103e2f0162daec317199de307ee2cfdb
Martin Apitz <it@artmin.de>
2015-05-07 06:37:23 +0200

    Made adjustments in shop. Added Delivery slip for production order.

--- dreh24_reports/__openerp__.py
+++ dreh24_reports/__openerp__.py
@@ -14,6 +14,8 @@ Reports for company dreh24 AG
         'views/dreh24_layout.xml',
         'views/dreh24_reports.xml',
         'views/weippert_production_report.xml',
+        'views/mrp_delivery.xml',
+        'mrp_delivery_report.xml',
     ],
     'installable': True,
     'auto-install' : False,
